c0dd9769148e4f736c783436c7e8278d685df64f
eoin <e.norris@mac.com>
2026-02-19 10:27:10 +0000

    Hook up ruler size setting to view model

--- MacRuler/Settings/SettingsView.swift
+++ MacRuler/Settings/SettingsView.swift
@@ -15,7 +15,7 @@ struct SettingsView: View {
             Form {
                 UnitsSettingsView(rulerSettingsViewModel: $rulerSettingsViewModel)
                 Divider()
-                RulerSettingsView()
+                RulerSettingsView(rulerSettingsViewModel: $rulerSettingsViewModel)
             }
         }
         .padding(20)
@@ -41,13 +41,40 @@ fileprivate struct UnitsSettingsView: View {
 }
 
 fileprivate struct RulerSettingsView: View {
-    @State private var rulerSize = "Normal"
+    @Binding var rulerSettingsViewModel: RulerSettingsViewModel
+
+    private enum RulerSizeOption: String {
+        case normal = "Normal"
+        case large = "Large"
+
+        var backgroundSize: RulerBackgroundSize {
+            switch self {
+            case .normal:
+                .small
+            case .large:
+                .large
+            }
+        }
+    }
+
+    private var rulerSizeBinding: Binding<RulerSizeOption> {
+        Binding(
+            get: {
+                rulerSettingsViewModel.horizontalRulerBackgroundSize == .large ? .large : .normal
+            },
+            set: { selectedSize in
+                let backgroundSize = selectedSize.backgroundSize
+                rulerSettingsViewModel.horizontalRulerBackgroundSize = backgroundSize
+                rulerSettingsViewModel.verticalRulerBackgroundSize = backgroundSize
+            }
+        )
+    }
 
     var body: some View {
         Section("Ruler") {
-            Picker("Ruler Size", selection: $rulerSize) {
-                Text("Normal").tag("Normal")
-                Text("Large").tag("Large")
+            Picker("Ruler Size", selection: rulerSizeBinding) {
+                Text(RulerSizeOption.normal.rawValue).tag(RulerSizeOption.normal)
+                Text(RulerSizeOption.large.rawValue).tag(RulerSizeOption.large)
             }
             .pickerStyle(.radioGroup)
             .padding(.top)
